de0300991f93aaf1713f8db61ae8956ab01cacd5
liudaoming <1213982008@qq.com.com>
2019-01-03 12:25:58 +0800

    first commit

---
+++ Assets/scripts/EasyIKBone.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasyIKBone : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    Vector3 startP;
+    [SerializeField]
+    Vector3 endP;
+    [SerializeField]
+    float boneLength = 0f;
+    [SerializeField]
+
+    public EasyIKBone parent=null;
+    public EasyIKBone child = null;
+
+    private Vector3 boneAxis = new Vector3(-1.0f, 0.0f, 0.0f);
+    private Transform org;
+    private Quaternion localRotation;
+    //private EasyIKManager managerRef;
+    private Vector3 targetDir;
+    public void Init(EasyIKBone parent_)
+    {
+        startP = transform.position;
+        localRotation = transform.localRotation;
+        //managerRef = manager;
+        parent = parent_;
+        if (parent != null)
+        {
+            parent.child = this;
+        }
+
+        if (child != null)
+        {
+            endP = child.transform.position;
+            boneLength = Vector3.Distance(startP, endP);
+        }
+        else
+        {
+            endP = startP;
+            boneLength = 0f;
+        }
+    }
+    public void SetStartP(Vector3 p)
+    {
+        startP = p;
+    }
+    public void AttachStartP()
+    {
+        SetStartP(parent.endP);
+    }
+    public void Follow()
+    {
+        
+         Follow(child.startP);
+        
+    }
+    public void Follow(Vector3 target)
+    {
+
+        targetDir = target - startP;
+        targetDir.Normalize();
+        Vector3 dir = targetDir;
+        dir.Scale(new Vector3(-1,-1,-1)*boneLength);
+        startP = target+dir;
+    }
+    public void CalculateEndP()
+    {
+        endP = startP + targetDir*boneLength;
+    }
+
+    public void UpdateRotation (float weight) {
+
+        // rotate from original direction to target direction.
+        org = transform;
+        transform.localRotation = Quaternion.identity * localRotation;
+        Vector3 aimVector = transform.TransformDirection(boneAxis);
+        Quaternion aimRotation = Quaternion.FromToRotation(aimVector, endP-startP);
+        Quaternion secondaryRotation = aimRotation * transform.rotation;
+        transform.rotation = Quaternion.Lerp(org.rotation, secondaryRotation, weight);
+        //transform.position = startP;
+    }
+}
---
+++ Assets/scripts/EasyIKManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EasyIKBone))]
+public class EasyIKManager : MonoBehaviour {
+
+    public float weight =1f;
+    public Transform target;
+    public uint chainLength = 1;
+    EasyIKBone end;
+    EasyIKBone start;
+    Vector3 rootPosOffset;
+    // Use this for initialization
+    private void Awake()
+    {
+        if (chainLength < 1)
+        {
+            chainLength = 1;
+        }
+    }
+    // initialize the esay ik bones
+    void Start () {
+        Transform p = transform.parent;
+        EasyIKBone current = gameObject.GetComponent<EasyIKBone>();
+        end = current;
+        start = current;
+		for(int i = 1; i <= chainLength; i++)
+        {
+            EasyIKBone parent;
+            if (chainLength == i)
+            {
+                if (p != null)
+                    parent = p.gameObject.GetComponent<EasyIKBone>();
+                else
+                    parent = null;
+            }else
+            {
+                if (p != null)
+                {
+                    parent = p.gameObject.GetComponent<EasyIKBone>();
+                    if(parent == null)
+                        parent = p.gameObject.AddComponent<EasyIKBone>();
+                    start = parent;
+                }
+                else
+                {
+                    parent = null;
+                }
+            }
+            current.Init(parent);
+            if(p == null)
+            {
+                break;
+            }
+            p = p.parent;
+            current = parent;
+            
+        }
+        if (start.transform.parent != null)
+            rootPosOffset = start.transform.position - start.transform.parent.position;
+        else
+            rootPosOffset = Vector3.zero;
+	}
+	
+
+    // calculate the target position and update the bones' rotation according to the position.
+	void LateUpdate () {
+
+        end.Follow(target.position);
+        end.CalculateEndP();
+        EasyIKBone next = end.parent;
+        while (next != null)
+        {
+            next.Follow();
+            next.CalculateEndP();
+            next = next.parent;
+        }
+        if (start.transform.parent!=null)
+            start.SetStartP(rootPosOffset + start.transform.parent.position);
+        else
+            start.SetStartP(start.transform.position);
+
+        start.CalculateEndP();
+        start.UpdateRotation(weight);
+        next = start.child;
+        
+        while (next != null)
+        {
+            next.AttachStartP();
+            next.CalculateEndP();
+            next.UpdateRotation(weight);
+            next = next.child;
+        }
+    }
+}
